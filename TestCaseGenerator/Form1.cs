e56eb7c4c84387ecaa9512fc6d3f1c453f1bb1a5
Aditya Mukkawar <aditya.mukkawar@accionlabs.com>
2023-02-02 13:22:50 +0530

    Initial commit

---
+++ TestCaseGenerator/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestCaseGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            this.textBox1.Text = openFileDialog1.FileName;
+        }
+    }
+}
